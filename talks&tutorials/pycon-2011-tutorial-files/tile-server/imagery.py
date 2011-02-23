a1d16fc272d398cc12604cd3f62022b855618c88
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-23 19:28:31 +0200

    More updates

--- talks&tutorials/pycon-2011-tutorial-files/tile-server/imagery.py
+++ talks&tutorials/pycon-2011-tutorial-files/tile-server/imagery.py
@@ -5,14 +5,32 @@ from flask import Flask
 import mapnik
 
 
+projection = mapnik.Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
+                               "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
+                               "+nadgrids=@null +no_defs +over")
+map = mapnik.Map(0, 0)
+mapnik.load_map(map, "map.xml")
 app = Flask(__name__)
-cache = {}
 
 
-def render(map, width, height):
+def render(map, bbox, width, height):
+    map
+    map.resize(width, height)
     image = mapnik.Image(width, height)
     mapnik.render(map, image)
     return image.tostring('png')
+
+
+def parse_coords(tile_size, meta_size, zoom, x, y):
+    """Transform tile offsets into queriable coordinates"""
+    coords = [(x, y), (x + meta_size, y + meta_size)]
+    coords = [[offset * float(tile_size) for offset in coord]
+              for coord in coords]
+    coords = [utils.pixel2latlon(coord, zoom, tile_size) for coord in coords]
+    coords = [mapnik.Coord(*coord) for coord in coords]
+    coords = [projection.forward(coord) for coord in coords]
+    return coords
+
 
 
 @app.route('/')
@@ -21,12 +39,15 @@ def main():
 
 
 @app.route('/<int:zoom>/<int:xtile>/<int:ytile>.<image_type>')
-def osm_tile(zoom, xtile, ytile, image_type):
+def tile(zoom, xtile, ytile, image_type):
     if image_type not in ('png', 'jpg'):
         raise
-    print zoom, xtile, ytile, image_type
-    return 'fuck yeah'
+    bbox =
+    return render(map, 256, 256)
 
+@app.route('/<style_id>/<int:zoom>/<int:xtile>/<int:ytile>.<image_type>')
+def styled_tile(style_id, zoom, xtile, ytile, image_type):
+    pass
 
 
 
