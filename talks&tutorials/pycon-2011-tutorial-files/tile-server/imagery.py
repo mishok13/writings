03fccd101c1836e73170ea68b827dc4aadaf3dff
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-24 18:41:35 +0200

    Smallish update

--- talks&tutorials/pycon-2011-tutorial-files/tile-server/imagery.py
+++ talks&tutorials/pycon-2011-tutorial-files/tile-server/imagery.py
@@ -65,7 +65,8 @@ def simple():
 def tile(zoom, xtile, ytile, image_type):
     if image_type not in ('png', 'jpg'):
         raise
-    bbox = parse_coords(256, 2, zoom, xtile, ytile)
+    metasize = 1 if zoom < 2 else 2
+    bbox = parse_coords(256, metasize, zoom, xtile, ytile)
     try:
         return Response(render(map, bbox, 256, 256), content_type='image/png')
     except Exception, exc:
