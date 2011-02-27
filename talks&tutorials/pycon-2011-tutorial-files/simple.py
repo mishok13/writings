529f4ff5cfd9b86ace5f02fa17ec12d4dbe0dad8
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-02-28 01:13:43 +0200

    Too much files

---
+++ talks&tutorials/pycon-2011-tutorial-files/simple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+
+import mapnik
+import sys
+
+print 'fuck'
+
+
+projection = mapnik.Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
+                               "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
+                               "+nadgrids=@null +no_defs +over")
+
+
+map = mapnik.Map(600, 600)
+mapnik.load_map(map, sys.argv[1])
+image = mapnik.Image(600, 600)
+coords = mapnik.Coord(-179.9999, -85), mapnik.Coord(179.9999, 85)
+bbox = mapnik.Envelope(projection.forward(coords[0]),
+                       projection.forward(coords[1]))
+print bbox
+map.zoom_to_box(bbox)
+mapnik.render(map, image)
+print sys.argv
+with open(sys.argv[2], 'w') as f:
+    print 'wtf'
+    f.write(image.tostring('png'))
