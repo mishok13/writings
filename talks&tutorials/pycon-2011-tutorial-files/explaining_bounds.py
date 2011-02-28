2ec40dbc0bf78376d3288a72b8909e2046813bac
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-28 17:29:20 +0200

    Updates mostly

--- talks&tutorials/pycon-2011-tutorial-files/explaining_bounds.py
+++ talks&tutorials/pycon-2011-tutorial-files/explaining_bounds.py
@@ -1,14 +1,15 @@
 from mapnik import render, Image, Map, load_map, Projection, Coord, Envelope
 from math import pi
 
-projection = Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
-                        "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
-                        "+nadgrids=@null +no_defs +over")
+projection = Projection(
+    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
+    "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
+    "+nadgrids=@null +no_defs +over")
 
 circumference = pi * 6378137
 
 map = Map(600, 600)
-load_map(map, "simple-rendering-with-xml.xml")
+load_map(map, "simple.xml")
 bbox = Envelope(Coord(-circumference, -circumference),
                 Coord(circumference, circumference))
 map.zoom_to_box(bbox)
--- talks&tutorials/pycon-2011-tutorial-files/simple_almost_fixed.py
+++ talks&tutorials/pycon-2011-tutorial-files/simple_almost_fixed.py
@@ -1,11 +1,13 @@
 from mapnik import render, Image, Map, load_map, Projection
 
-projection = Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
-                        "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
-                        "+nadgrids=@null +no_defs +over")
+projection = Projection(
+    "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
+    "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
+    "+nadgrids=@null +no_defs +over")
 
 map = Map(600, 600)
-load_map(map, "simple-rendering-with-xml.xml")
+load_map(map, "simple.xml")
+
 bbox = map.layers[0].envelope()
 map.zoom_to_box(bbox.forward(projection))
 image = Image(600, 600)
