591a2c96ca527d3d38d312a318a3e00229f4086e
Andrii V. Mishkovskyi <amishkovskiy@cloudmade.com>
2011-02-28 15:25:25 +0200

    Short updates in handouts

--- talks&tutorials/pycon-2011-tutorial-files/simple-rendering.py
+++ talks&tutorials/pycon-2011-tutorial-files/simple-rendering.py
@@ -1,33 +1,19 @@
-from mapnik import (Map, Image, Envelope, render, Rule, Color,
-                    Layer, Shapefile,
-                    Style, PolygonSymbolizer, LineSymbolizer)
+from mapnik import *
 
-
-def create_style(rule):
-    rule = Rule()
-    rule.symbols.append(PolygonSymbolizer(Color("#f2eff9")))
-    rule.symbols.append(LineSymbolizer(Color("black"), 0.1))
-    style = Style()
-    style.rules.append(rule)
-    return style
-
-
-def create_layer(style_name):
-    layer = Layer('world')
-    layer.datasource = Shapefile(file='path/to/shapefile')
-    layer.styles.append(style_name)
-    return layer
-
-
-def create_map():
-    m = Map(800, 400)
-    m.append_style('base', create_style)
-
-
+rule = Rule()
+rule.symbols.append(PolygonSymbolizer(Color("#f2eff9")))
+rule.symbols.append(LineSymbolizer(Color("grey"), 0.1))
+style = Style()
+style.rules.append(rule)
 m = Map(800, 400)
-m.append_style(style)
+layer = Layer('world')
+layer.datasource = Shapefile(file='tile-server/coastlines/110m_land')
+layer.styles.append('world')
+m.append_style('world', style)
 m.layers.append(layer)
 m.zoom_to_box(layer.envelope())
+print layer.envelope()
 image = Image(800, 400)
 render(m, image)
-image.tostring('png8')
+with open('test.png', 'w') as map:
+    map.write(image.tostring('png8'))
