abc91cf5143ef8c8e8c8663cafc48bcf8a20d4a9
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2011-03-02 19:59:43 +0200

    More updates

--- talks&tutorials/pycon-2011-tutorial-files/simple.py
+++ talks&tutorials/pycon-2011-tutorial-files/simple.py
@@ -4,24 +4,22 @@
 import mapnik
 import sys
 
-print 'fuck'
+projections = {'mercator': ("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
+                            "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
+                            "+nadgrids=@null +no_defs +over"),
+               'mollweide': ("+proj=moll +x_0=0 +y_0=0 +a=6371000"
+                             "+b=6371000 +units=m +no_defs")}
+
+projection = mapnik.Projection(projections['mollweide'])
 
 
-projection = mapnik.Projection("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 "
-                               "+lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m "
-                               "+nadgrids=@null +no_defs +over")
-
-
-map = mapnik.Map(600, 600)
+map = mapnik.Map(1200, 600)
 mapnik.load_map(map, sys.argv[1])
-image = mapnik.Image(600, 600)
-coords = mapnik.Coord(-179.9999, -85), mapnik.Coord(179.9999, 85)
+image = mapnik.Image(1200, 600)
+coords = mapnik.Coord(-180.0, -89.99999), mapnik.Coord(180.0, 89.99999)
 bbox = mapnik.Envelope(projection.forward(coords[0]),
                        projection.forward(coords[1]))
-print bbox
 map.zoom_to_box(bbox)
 mapnik.render(map, image)
-print sys.argv
 with open(sys.argv[2], 'w') as f:
-    print 'wtf'
     f.write(image.tostring('png'))
